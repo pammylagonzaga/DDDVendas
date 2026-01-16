c412ec7c56c9d5887c45285b93352fed0ec21c89
Pammyla Gonzaga <pammyla.gonzaga@gmail.com>
2026-01-16 10:06:45 -0300

    AulaXXI - Implementando RemoverPedidoCommand

---
+++ VendasApplication/Commands/Pedidos/RemoverItemDoPedido/RemoverItemDoPedidoResultDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendas.Application.Commands.Pedidos.RemoverItemDoPedido;
+public sealed class RemoverItemDoPedidoResultDto
+{
+    public Guid PedidoId { get; }
+    public decimal ValorTotal { get; }
+    public string Status { get; }
+
+    public RemoverItemDoPedidoResultDto(
+        Guid pedidoId,
+        decimal valorTotal,
+        string status)
+    {
+        PedidoId = pedidoId;
+        ValorTotal = valorTotal;
+        Status = status;
+    }
+}
