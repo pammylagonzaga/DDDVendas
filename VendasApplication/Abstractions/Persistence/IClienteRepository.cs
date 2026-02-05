b989c717b1c75da3b7b7a9f1b83be03b6f2690da
Pammyla Gonzaga <pammyla.gonzaga@gmail.com>
2026-02-05 16:35:41 -0300

    Aula XXIV

---
+++ VendasApplication/Abstractions/Persistence/IClienteRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vendas.Domain.Catalogo.Entities;
+using Vendas.Domain.Clientes.Entities;
+
+namespace Vendas.Application.Abstractions.Persistence;
+
+public interface IClienteRepository
+{
+    Task<Cliente?> ObterPorIdAsync(
+    Guid clienteId,
+    CancellationToken cancellationToken = default);
+    Task AdicionarAsync(
+        Cliente cliente,
+        CancellationToken cancellationToken = default);
+
+    Task AtualizarAsync(
+        Cliente cliente,
+        CancellationToken cancellationToken = default);
+}
---
+++ VendasApplication/Abstractions/Persistence/IProdutoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vendas.Domain.Catalogo.Entities;
+using Vendas.Domain.Pedidos.Entities;
+
+namespace Vendas.Application.Abstractions.Persistence;
+
+public interface IProdutoRepository
+{
+    Task<Produto?> ObterPorIdAsync(
+        Guid produtoId,
+        CancellationToken cancellationToken = default);
+    Task AdicionarAsync(
+        Produto produto,
+        CancellationToken cancellationToken = default);
+
+    Task AtualizarAsync(
+        Produto produto,
+        CancellationToken cancellationToken = default);
+}
